6ca54be413c7879e4d063ae3cdc51fd205f9dffc
AlexG-A01234719 <aguo11@my.bcit.ca>
2022-03-29 13:03:10 -0700

    processing volume updates

--- processing/app.py
+++ processing/app.py
@@ -16,6 +16,36 @@ from stats import Stats
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
+import sqlite3 
+
+if os.path.exists("/data/data.sqlite"):
+    conn = sqlite3.connect('stats.sqlite') 
+    
+    c = conn.cursor() 
+    c.execute(''' 
+            CREATE TABLE stats 
+            (id INTEGER PRIMARY KEY ASC,  
+            num_fi_entries INTEGER, 
+            total_fi_calorie INTEGER, 
+            num_di_entries INTEGER, 
+            total_di_calorie INTEGER, 
+            last_updated VARCHAR(100) NOT NULL) 
+            ''') 
+
+    c.execute("""
+            INSERT INTO stats(
+            num_fi_entries,
+            total_fi_calorie,
+            num_di_entries,
+            total_di_calorie,
+            last_updated)
+            VALUES(0,0,0,0, '1000-01-01 01:00:00');
+            """)
+
+    conn.commit() 
+    conn.close() 
+
+
 url = 'http://localhost:8100'
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
