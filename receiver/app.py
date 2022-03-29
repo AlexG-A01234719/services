8b53e6c7b71c43b126c56788664c560cb08b3c89
AlexG-A01234719 <aguo11@my.bcit.ca>
2022-03-29 12:32:20 -0700

    modify the code that loads in the application and log configurations in the app.py

--- receiver/app.py
+++ receiver/app.py
@@ -8,19 +8,34 @@ import logging
 import logging.config
 import uuid
 import time
+import os
 
 from pykafka import KafkaClient
 
+
 url = 'http://localhost:8090'
 
-with open('app_conf.yml', 'r') as f: 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+    
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
     
-with open('log_conf.yml', 'r') as f: 
-    log_config = yaml.safe_load(f.read()) 
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 current_retry = 0
 
